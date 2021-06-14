6ad7d673de8faf45d9ff97123b10ecfa021afc75
TechPro <phineasm4065@utahvirtual.org>
2021-06-14 14:05:45 -0600

    Added PWA resources

---
+++ serviceWorker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('TechPro-Media').then((cache) => cache.addAll([
+      '/',
+      'Styles/main.css',
+      'Images/green_background.jpg',
+      'Images/logo_new.svg',
+      'Images/logo_new.png',
+      'Scripts/main.js',
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
